7861997773424f3874154057b1cda5e6f72b5e44
Nobikun10 <97254409+Nobikun10@users.noreply.github.com>
2023-09-29 09:13:24 +0530

    Update insertionsort.java

--- insertionsort.java
+++ insertionsort.java
@@ -1,1 +1,13 @@
-
+public static void insertionsort(int num[]){
+        //find the correct position to insert
+        for(int i=1; i<num.length; i++){
+            int curr = num[i];
+            int prev = i-1;
+            while(prev >= 0 && curr < num[prev]){
+                num[prev+1] = num[prev];
+                prev--;
+            }
+            num[prev+1] = curr;
+        }
+        
+    }
